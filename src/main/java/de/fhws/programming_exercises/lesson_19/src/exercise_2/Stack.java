db671fd81bcb025b7f45231a6c5fae12653cabb0
Matija Cabadaj <mcabadaj201@gmail.com>
2022-06-01 17:34:04 +0200

    lesson 20 done

---
+++ src/main/java/de/fhws/programming_exercises/lesson_19/src/exercise_2/Stack.java
@@ -0,0 +1,39 @@
+package de.fhws.programming_exercises.lesson_19.src.exercise_2;
+
+import java.util.ArrayList;
+
+public final class Stack{
+
+    private final ArrayList<Integer> aList = new ArrayList<>();
+
+    public int getSize(){
+        return aList.size();
+    }
+
+    public void push(int number){
+        aList.add(number);
+    }
+
+    /**
+     * Retrieves the last element in the stack and removes it.
+     *
+     * @return the popped value
+     *
+     * @throws IndexOutOfBoundsException when user tries to pop from an empty stack
+     */
+    public int pop(){
+        int last = aList.size() - 1;
+        if (last < 0){
+            throw new IndexOutOfBoundsException("Cannot pop from empty stack");
+        }
+        int var = aList.get(last);
+        aList.remove(last);
+        return var;
+    }
+
+    @Override
+    public String toString(){
+        return aList.toString();
+    }
+
+}
---
+++ src/main/java/de/fhws/programming_exercises/lesson_19/src/exercise_2/StackVererbung.java
@@ -0,0 +1,8 @@
+package de.fhws.programming_exercises.lesson_19.src.exercise_2;
+
+
+//Klasse zum Upload ins e-learning
+
+public class StackVererbung{
+
+}
